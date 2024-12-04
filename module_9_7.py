394119f681af7abe532ee4064a2bd59d2e73e442
YuliyaGorbacheva <Ulechka782@yandex.ru>
2024-12-04 16:36:53 +0400

    Add files via upload

--- module_9_7.py
+++ module_9_7.py
@@ -1,19 +1,23 @@
 def is_prime(func):
     def wrapper(*args):
         result = func(*args)
-        for i in range(2,result):
+        is_prime = True
+        for i in range(2, int(result ** 0.5) + 1):
             if result % i == 0:
-                return (f'Простое \n{result}')
-            else:
-                return (f'Составное \n{result}')
+                is_prime = False
+        if is_prime == True:
+            return (f'Простое\n{result}')
+
+        else:
+            return (f'Составное\n{result}')
 
     return wrapper
 
 
 @is_prime
-def sum_three(i,j,k):
-    return i+j+k
+def sum_three(i, j, k):
+    return i + j + k
 
 
-result = sum_three(2, 3, 6)
+result = sum_three(2, 3, 0)
 print(result)
